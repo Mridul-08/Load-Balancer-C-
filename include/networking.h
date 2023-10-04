7d36f97c83727bf21217e74ae3ff969d3c881344
Mridul-08 <83511341+Mridul-08@users.noreply.github.com>
2023-10-04 19:27:34 +0530

    created a struct for socket connection

---
+++ include/networking.h
@@ -0,0 +1,37 @@
+//make a socket connection
+#ifndef NETWORKING_H
+#define NETWORKING_H
+
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+struct socket_name_info{
+    char host[INET6_ADDRSTRLEN];
+    char port[NI_MAXSERV];
+};
+
+struct socket_connection {
+    int socket_fd;
+    struct sockaddr_in address;
+    struct socket_name_info socket_name;
+};
+
+
+int get_socket(struct socket_connection *server_socket, char *address, unsigned int port);
+int bind_to_socket(struct socket_connection server_socket);
+int connect_to_socket(struct socket_connection server_socket);
+int listen_to_socket(struct socket_connection serer_socket, unsigned int backlog);
+
+
+int hostname_to_ip(char *hostname, unsigned int port, char *ip);
+
+
+
+
+
+#endif
+//NETWORKING_H
---
+++ src/main.c
@@ -0,0 +1,14 @@
+#include <arpa/inet.h>
+#include <errno.h>
+#include <netdb.h>
+#include <netinet/in.h>
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]){
+
+}
